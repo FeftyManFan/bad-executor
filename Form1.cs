6a5431e30b6ccb012f9e2b33a09331490cd8d698
FeftyManFan <127222287+FeftyManFan@users.noreply.github.com>
2023-03-18 17:27:38 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FastXploits;
+
+namespace _2MinuteWare
+{
+    public partial class Form1 : Form
+    {
+        FastAPI api = new FastAPI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            api.Execute(richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            api.Inject();
+        }
+    }
+}
